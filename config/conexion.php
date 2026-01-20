beb00b7a931db1d87657d308877cf5f647868022
bryancorona221998-source <bryancorona221998@gmail.com>
2026-01-19 22:02:20 -0600

    Add files via upload

--- config/conexion.php
+++ config/conexion.php
@@ -1,1 +1,21 @@
+<?php
+// 1. Establecer la zona horaria correcta para Guatemala
+date_default_timezone_set('America/Guatemala');
 
+// Datos de conexión
+$host = "localhost";
+$usuario = "root";
+$clave = ""; 
+$bd = "amanecer_cientifico";
+
+// Crear conexión
+$conexion = mysqli_connect($host, $usuario, $clave, $bd);
+
+// Verificar conexión
+if (!$conexion) {
+    die("Error fatal: No se pudo conectar. " . mysqli_connect_error());
+}
+
+// Configurar caracteres (para que las tildes y ñ se vean bien)
+mysqli_set_charset($conexion, "utf8");
+?>
